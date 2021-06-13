5389b981bcfc66b1af06b7610910d5e7d6916128
Pinot Site Updater <dev@pinot.apache.org>
2021-06-13 00:09:06 +0000

    Update Pinot Site from dev branch c08591f6964ef2bfe4b04a6b686ecbe8dd5c12de
    commit c08591f6964ef2bfe4b04a6b686ecbe8dd5c12de
    Merge: 762d3b0 898cb66
    Author: Mayank Shrivastava <mayanks@apache.org>
    Date:   Sat Jun 12 17:06:35 2021 -0700
    
        Merge pull request #47 from ChethanUK/f/who-uses
    
        Adding tags and Updating who_uses apache pinot page
    
    commit 898cb66ed4e47d9861dc226793e6e281610ef16c
    Author: ChethanUK <chethanuk@outlook.com>
    Date:   Sat Jun 12 20:35:45 2021 +0530
    
        Updating who uses apache pinot

---
+++ content/assets/js/8eb4e46b.7b80c4b0.js
@@ -0,0 +1,1 @@
+(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1],{1421:function(e){"use strict";e.exports=JSON.parse('{"permalink":"/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":14,"previousPage":"/blog","nextPage":null,"blogDescription":"Blog","blogTitle":"Blog - Apache Pinot: User-Facing Analytics"}')}}]);
