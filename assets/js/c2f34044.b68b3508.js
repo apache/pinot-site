a9a2116ae4ead5917f1085bb1b3b11b9fb522491
Pinot Site Updater <dev@pinot.apache.org>
2022-04-04 23:08:36 +0000

    Update Pinot Site from dev branch 0b5afd1662621f906385bf9116b8c9623dd9536a
    commit 0b5afd1662621f906385bf9116b8c9623dd9536a
    Author: Kenny Bastani <kennybastani@gmail.com>
    Date:   Mon Apr 4 18:58:28 2022 -0400
    
        Pinot 0.10.0 Announcement (#72)
    
        * Pinot 0.10.0 Announcement
    
        * Pinot 0.10.0 Announcement
    
        * Fixing image deadlinks on blogs
    
    commit 9946b1b30a0742a0c727648aa269063b173ad290
    Author: Xiang Fu <xiangfu.1024@gmail.com>
    Date:   Tue Mar 29 01:39:19 2022 -0700
    
        update about pages

---
+++ assets/js/c2f34044.b68b3508.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[5457],{2379:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"releases","name":"releases","count":1,"permalink":"/blog/tags/releases"}')}}]);
