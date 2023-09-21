58baf315e8ab35f23eb57ad9252bf0eae08bbac1
Tim Berglund <tlberglund@gmail.com>
2023-09-21 16:28:18 -0600

    Experiments with dependencies

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -215,16 +215,6 @@ module.exports = {
     },
   },
   plugins: [
-    [
-      '@docusaurus/plugin-ideal-image',
-      // '@docusaurus/plugin-content-blog',
-      {
-        quality: 70,
-        max: 1030, // max resized image's size.
-        min: 640, // min resized image's size. if original is lower, use that size.
-        steps: 2, // the max number of images generated between min and max (inclusive)
-      },
-    ],
   ],
   presets: [
     [
