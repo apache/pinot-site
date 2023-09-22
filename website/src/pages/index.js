d46c704c26aa8d11a83e64434c8e07e21186ae21
Xiang Fu <xiangfu.1024@gmail.com>
2023-09-22 14:25:52 -0700

    Update index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -300,7 +300,7 @@ function Installation() {
                     </TabItem>
                     <TabItem value="binary">
                         <CodeBlock className="language-bash">
-                            {`VERSION=0.12.1\nwget https://downloads.apache.org/pinot/apache-pinot-$VERSION/apache-pinot-$VERSION-bin.tar.gz\ntar vxf apache-pinot-*-bin.tar.gz\ncd apache-pinot-*-bin\nbin/quick-start-batch.sh`}
+                            {`VERSION=1.0.0\nwget https://downloads.apache.org/pinot/apache-pinot-$VERSION/apache-pinot-$VERSION-bin.tar.gz\ntar vxf apache-pinot-*-bin.tar.gz\ncd apache-pinot-*-bin\nbin/quick-start-batch.sh`}
                         </CodeBlock>
                     </TabItem>
                     <TabItem value="github">
@@ -367,11 +367,11 @@ function Home() {
             >
                 <div className="container">
                     <Link
-                        to="https://docs.pinot.apache.org/basics/releases/0.12.1"
+                        to="https://docs.pinot.apache.org/basics/releases/1.0.0"
                         className={styles.indexAnnouncement}
                     >
                         <span className="badge badge-primary">release</span>
-                        v0.12.1 has been released! Check the release notes
+                        v1.0.0 has been released! Check the release notes
                     </Link>
                     <h1 className="hero__title">{siteConfig.title}</h1>
                     <p className="hero__subtitle">
