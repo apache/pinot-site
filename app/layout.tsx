92ab35db83b50ba3248c9417f0ffe42de9d28e26
Gio <gio@startree.ai>
2024-04-08 14:21:27 +0200

    Add frame-src header using meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,6 +88,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     href="/static/favicons/safari-pinned-tab.svg"
                     color="#5bbad5"
                 />
+
                 <meta
                     http-equiv="Content-Security-Policy"
                     content="frame-src 'self' https://www.youtube.com;"
