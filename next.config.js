c19024cbc1df8cd905f0c63e1d63b8aac745e7aa
Xiang Fu <xiangfu.1024@gmail.com>
2024-04-09 16:55:01 +0800

    Remove meta from layout.tsx

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src 'self' giscus.app youtu.be youtube.com *.youtube.com googlevideo.com *.googlevideo.com;
+  frame-src 'self' giscus.app youtu.be youtube.com www.youtube.com
 `;
 
 const securityHeaders = [
