278bf8c983adbe5656b8ea54b10c1834e4bf4faa
Gio <153032991+gio-startree@users.noreply.github.com>
2024-04-09 14:42:52 +0200

    Wb 280 add webex (#111)
    * Add frame-src header using meta tag
    
    * Fix meta tag frame-src for youtube
    
    * - Add Webex;
    - Update favicon;
    - Fix logos not working in Powered By page;
    - Remove Uber duplicate;
    - Add placeholder for YouTube videos while finding a solution.

--- components/VideoEmbed.tsx
+++ components/VideoEmbed.tsx
@@ -1,19 +1,48 @@
+'use client';
+
+import { useState } from 'react';
+
 type VideoEmbedProps = {
     src: string;
     title?: string;
 };
 
 const VideoEmbed = ({ src, title }: VideoEmbedProps) => {
+    const [iframeLoaded, setIframeLoaded] = useState(false);
+    const imagePlaceHolder = '/static/images/video_thumbnail.png';
+
+    const handlePlaceholderClick = () => {
+        setIframeLoaded(true);
+    };
+
+    const handleKeyPress = (event) => {
+        // Check if the key pressed is 'Enter' or 'Space'
+        if (event.key === 'Enter' || event.key === ' ') {
+            setIframeLoaded(true);
+        }
+    };
+
     return (
         <div className="aspect-h-9 aspect-w-16">
-            <iframe
-                className="h-full w-full"
-                src={src}
-                title={title || 'Embedded Video'}
-                allowFullScreen
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            ></iframe>
+            {!iframeLoaded ? (
+                <div
+                    className="video-placeholder flex h-full cursor-pointer items-center justify-center bg-cover bg-center"
+                    onClick={handlePlaceholderClick}
+                    onKeyPress={handleKeyPress}
+                    role="button"
+                    tabIndex={0} // Make it focusable
+                    style={{ cursor: 'pointer', backgroundImage: `url(${imagePlaceHolder})` }}
+                />
+            ) : (
+                <iframe
+                    className="h-full w-full"
+                    src={src}
+                    title={title || 'Embedded Video'}
+                    allowFullScreen
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                ></iframe>
+            )}
         </div>
     );
 };
--- data/companyQuotes.ts
+++ data/companyQuotes.ts
@@ -10,13 +10,13 @@ const quotes = [
         company: 'Stripe',
         text: 'Pinot enables us to execute sub-second, petabyte-scale aggregation queries over fresh financial events in our internal ledger. We chose Pinot because of its rich feature set and scalability, which has enabled better performance than our previous solution — at a lower cost.',
         author: 'Peter Bakkum'
+    },
+    {
+        logo: '/static/images/carousel/webex.svg',
+        company: 'Webex',
+        text: 'Forget sluggish queries!! Apache Pinot whipped our runtime aggregates, with sub-second latencies on all but the most complex queries. On top of the speed boost, Pinot slashed our storage footprint by 10x, letting us shrink the cluster by a whopping 500 nodes',
+        author: 'WEBEX'
     }
-    // {
-    //     logo: '/static/images/carousel/razorpay.svg',
-    //     company: 'Company 3',
-    //     text: "Apache Pinot has truly played a fundamental role in Razorpay's monitoring journey!",
-    //     author: 'Author 3'
-    // }
 ];
 
 export default quotes;
