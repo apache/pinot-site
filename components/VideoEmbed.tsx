c95d7fb4cb6302b5459e1477c4ec980fe85319e8
Gio <gio@startree.ai>
2024-04-16 16:07:20 +0200

    - Remove ReactPlayer, - Code cleanup, Fix videos

--- components/VideoEmbed.tsx
+++ components/VideoEmbed.tsx
@@ -1,60 +1,21 @@
 'use client';
 
-import { useState } from 'react';
-
 type VideoEmbedProps = {
     src: string;
     title?: string;
 };
 
 const VideoEmbed = ({ src, title }: VideoEmbedProps) => {
-    const [iframeLoaded, setIframeLoaded] = useState(false);
-    const imagePlaceholder = '/static/images/video_thumbnail.png';
-
-    const handlePlaceholderClick = () => {
-        setIframeLoaded(true);
-        window.open(src, '_blank');
-    };
-
-    const handleKeyPress = (event) => {
-        // Check if the key pressed is 'Enter' or 'Space'
-        if (event.key === 'Enter' || event.key === ' ') {
-            setIframeLoaded(true);
-            window.open(src, '_blank');
-        }
-    };
-
     return (
         <div className="aspect-h-9 aspect-w-16">
-            <div
-                className="video-placeholder flex h-full cursor-pointer items-center justify-center bg-cover bg-center"
-                onClick={handlePlaceholderClick}
-                onKeyPress={handleKeyPress}
-                role="button"
-                tabIndex={0} // Make it focusable
-                style={{ backgroundImage: `url(${imagePlaceholder})` }}
-                aria-label={title || 'Open video in new tab'} // Improving accessibility
-            />
-
-            {/* {!iframeLoaded ? (
-                <div
-                    className="video-placeholder flex h-full cursor-pointer items-center justify-center bg-cover bg-center"
-                    onClick={handlePlaceholderClick}
-                    onKeyPress={handleKeyPress}
-                    role="button"
-                    tabIndex={0} // Make it focusable
-                    style={{ cursor: 'pointer', backgroundImage: `url(${imagePlaceHolder})` }}
-                />
-            ) : (
-                <iframe
-                    className="h-full w-full"
-                    src={src}
-                    title={title || 'Embedded Video'}
-                    allowFullScreen
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                ></iframe>
-            )} */}
+            <iframe
+                className="h-full w-full"
+                src={src}
+                title={title || 'Embedded Video'}
+                allowFullScreen
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
         </div>
     );
 };
