6aeee96daee2b3c8e9d23a791bd871679e3f154e
Gio <gio@startree.ai>
2024-04-17 18:56:35 +0200

    Add favicon sizes 32x32, 16x16, apple-touch-icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,19 +73,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link
                     rel="apple-touch-icon"
                     sizes="76x76"
-                    href="/static/favicons/apple-touch-icon.svg"
+                    href="/static/favicons/apple-touch-icon.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="32x32"
-                    href="/static/favicons/favicon-32x32.svg"
+                    href="/static/favicons/favicon-32x32.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="16x16"
-                    href="/static/favicons/favicon-16x16.svg"
+                    href="/static/favicons/favicon-16x16.png"
                 />
                 <link rel="manifest" href="/static/favicons/site.webmanifest" />
                 <link
