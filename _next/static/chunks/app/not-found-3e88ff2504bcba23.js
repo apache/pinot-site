c4bde2e52af835e02449d47147b452674be0f700
Pinot Site Updater <dev@pinot.apache.org>
2024-04-20 02:18:58 +0000

    Update Pinot Site from dev branch 8af5f02150285f0e8049cacf7d47226341ab7e31
    commit 8af5f02150285f0e8049cacf7d47226341ab7e31
    Author: Gio <153032991+gio-startree@users.noreply.github.com>
    Date:   Sat Apr 20 04:17:06 2024 +0200
    
        no-ticket - remove tags, fix sitemap (#120)
    
    commit 75f2b0e1748b1c9cba9f819f3961e97f0c935300
    Author: Gio <153032991+gio-startree@users.noreply.github.com>
    Date:   Wed Apr 17 20:43:58 2024 +0200
    
        WB-283 - Capitalise content across the site (#119)

---
+++ _next/static/chunks/app/not-found-3e88ff2504bcba23.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,404,40],{2844:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=2844)}),_N_E=n.O()}]);
