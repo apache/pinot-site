c78247a46894c8041cdee4c3388ec3cde62b530d
Gio <153032991+gio-startree@users.noreply.github.com>
2024-08-21 13:24:53 +0200

    add trailingSlash in next config to handle the trailing slash in the url (#132)

--- next.config.js
+++ next.config.js
@@ -61,6 +61,7 @@ module.exports = () => {
     const plugins = [withContentlayer, withBundleAnalyzer];
     return plugins.reduce((acc, next) => next(acc), {
         output: 'export',
+        trailingSlash: true,
         // distDir: 'dist',
         reactStrictMode: true,
         pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
