bf7f5d075f946fe83c18be118d5430b8068fc02a
Xiang Fu <xiangfu.1024@gmail.com>
2026-03-20 23:15:24 -0700

    Rename 'Powered by' to 'Who Uses Pinot' across the site (#162)
    - Update navigation link title in headerNavLinks.ts
    - Update page metadata title in powered-by/page.tsx
    - Update main heading in powered-by/page.tsx
    - Keep URL path unchanged to avoid breaking existing links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
     { href: 'https://docs.pinot.apache.org', title: 'Docs' },
     { href: '/use-cases/', title: 'Use Cases' },
     { href: '/download/', title: 'Download' },
-    { href: '/powered-by/', title: 'Powered by' },
+    { href: '/powered-by/', title: 'Who Uses Pinot' },
     { href: '/blog/', title: 'Blog' },
     { href: '/#join-community', title: 'Community' }
 ];
